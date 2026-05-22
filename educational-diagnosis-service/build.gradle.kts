c76af338158ac705be9cb15fbd813c4cbfc7bebc
jamartinezv2023 <jamartinezv2023@udea.edu.co>
2026-05-22 13:40:01 -0500

    feat(diagnosis): add educational diagnosis service foundation

---
+++ educational-diagnosis-service/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("org.springframework.boot")
+}
+
+dependencies {
+    implementation(project(":common-events"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
