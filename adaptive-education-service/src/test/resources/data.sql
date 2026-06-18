cc416e8199cf26ab6178e8f90649a9ba85ff6504
jamartinezv2023 <jamartinezv2023@udea.edu.co>
2026-06-17 23:10:50 -0500

    test(adaptive): stabilize H2 test profile for CI

--- adaptive-education-service/src/test/resources/data.sql
+++ adaptive-education-service/src/test/resources/data.sql
@@ -1,5 +1,13 @@
 DELETE FROM adaptation_rules;
-INSERT INTO adaptation_rules (style_key, style_value, adjustment_content) VALUES ('kolb', 'CONVERGENT', 'Priorizar guías de laboratorio y resolución de problemas técnicos.');
-INSERT INTO adaptation_rules (style_key, style_value, adjustment_content) VALUES ('kolb', 'DIVERGENT', 'Fomentar mapas mentales y foros de discusión sobre casos de estudio.');
-INSERT INTO adaptation_rules (style_key, style_value, adjustment_content) VALUES ('kolb', 'ASSIMILATOR', 'Proveer textos científicos detallados y modelos teóricos abstractos.');
-INSERT INTO adaptation_rules (style_key, style_value, adjustment_content) VALUES ('kolb', 'ACCOMMODATOR', 'Asignar proyectos de campo y simulaciones basadas en la acción.');
+
+INSERT INTO adaptation_rules (style_key, style_value, adjustment_content)
+VALUES ('kolb', 'CONVERGENT', 'Priorizar guias de laboratorio y resolucion de problemas tecnicos.');
+
+INSERT INTO adaptation_rules (style_key, style_value, adjustment_content)
+VALUES ('kolb', 'DIVERGENT', 'Fomentar mapas mentales y foros de discusion sobre casos de estudio.');
+
+INSERT INTO adaptation_rules (style_key, style_value, adjustment_content)
+VALUES ('kolb', 'ASSIMILATOR', 'Proveer textos cientificos detallados y modelos teoricos abstractos.');
+
+INSERT INTO adaptation_rules (style_key, style_value, adjustment_content)
+VALUES ('kolb', 'ACCOMMODATOR', 'Asignar proyectos de campo y simulaciones basadas en la accion.');
