f98bc962f8ac2efdb53ee6cb62d8b8d97f9e2e42
jamartinezv2023 <jamartinezv2023@udea.edu.co>
2026-08-06 01:29:49 -0500

    quality(adaptive): complete checkstyle release compliance

--- adaptive-education-service/src/test/java/com/inclusive/adaptiveeducationservice/assessmentengine/generic/application/AssessmentDefinitionServiceTest.java
+++ adaptive-education-service/src/test/java/com/inclusive/adaptiveeducationservice/assessmentengine/generic/application/AssessmentDefinitionServiceTest.java
@@ -17,8 +17,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class AssessmentDefinitionServiceTest {
 
     @Test
